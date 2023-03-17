ac50acd515b6f74c375bc560fb468734aa203cf3
Azrenbeth <77782548+Azrenbeth@users.noreply.github.com>
2023-03-17 10:56:27 +0000

    Fix import cycle and copy bhshapes to graph gen

--- src/gcf.py
+++ src/gcf.py
@@ -2,9 +2,10 @@ import networkx as nx
 import torch as t
 import torch_geometric as pyg
 import gspan_mining as gspan
-from gcf import Graph, subgraph
+from graph import Graph, subgraph
 
 # WARNING: UNTESTED CODE
+
 
 def concept_purity(graph, concept):
     """Returns the graph_edit_distance of all graphs in the concept.
@@ -16,8 +17,7 @@ def concept_purity(graph, concept):
     for node_set_a in concept:
         for node_set_b in concept:
             ged = nx.graph_edit_distance(
-                subgraph(graph, node_set_a),
-                subgraph(graph, node_set_b)
+                subgraph(graph, node_set_a), subgraph(graph, node_set_b)
             )
             sum_ged += ged
             pairs += 1
@@ -25,31 +25,28 @@ def concept_purity(graph, concept):
 
 
 class GraphConceptFinder:
-    def __init__(
-        self,
-        graph_to_concepts_fn
-    ):
+    def __init__(self, graph_to_concepts_fn):
         self.graph_to_concepts_fn = graph_to_concepts_fn
-        self.concepts = None # the last call to find_concepts is stored here
-        self.graph = None # store the last graph passed to find_concepts
-    
+        self.concepts = None  # the last call to find_concepts is stored here
+        self.graph = None  # store the last graph passed to find_concepts
+
     def find_concepts(self, graph):
         concepts = self.graph_to_concepts_fn(graph)
         self.concepts = concepts
         # each concept is a set of nodes in the graph
         return concepts
-    
+
     def save_concepts(self, graph, file):
         concepts = self.find_concepts(graph)
         # create file if it doesn't exist, overwrite if it does:
         t.save(concepts, file)
         return concepts
-    
+
     def load_concepts(self, file):
         concepts = t.load(file)
         self.concepts = concepts
         return concepts
-    
+
     def concept_purities(self):
         purities = []
         for concept in self.concepts:
@@ -58,5 +55,5 @@ class GraphConceptFinder:
 
 
 gspan_concept_finder = GraphConceptFinder(
-    lambda graph : gspan(graph.to_gspan())
+    lambda graph: gspan(graph.to_gspan())
 )
--- src/graph_gen.py
+++ src/graph_gen.py
@@ -7,30 +7,39 @@ from gcf import Graph
 import torch as t
 import torch_geometric as pyg
 
-def house():
-    edges = [
-        [0, 1],
-        [0, 3],
-        [0, 4],
-        [1, 4],
-        [1, 2],
-        [1, 0],
-        [2, 1],
-        [2, 3],
-        [3, 2],
-        [3, 0],
-        [4, 0],
-        [4, 1]
-    ]
+
+def _raw_house():
+    edges = np.array(
+        [
+            [0, 1],
+            [0, 3],
+            [0, 4],
+            [1, 4],
+            [1, 2],
+            [1, 0],
+            [2, 1],
+            [2, 3],
+            [3, 2],
+            [3, 0],
+            [4, 0],
+            [4, 1],
+        ]
+    )
     labels = [1, 1, 2, 2, 3]
-    return Graph((labels, edges))
+    return (edges, labels)
+
+
+def house():
+    e, l = _raw_house()
+    return Graph(list(l), e)
+
 
 def triangle():
     # create networkx triangle graph:
     return Graph(
         pyg.data.Data(
-            x = t.tensor([0, 1, 2]),
-            edge_index = t.tensor([[0, 1, 2, 0, 1, 2], [1, 2, 0, 2, 0, 1]])
+            x=t.tensor([0, 1, 2]),
+            edge_index=t.tensor([[0, 1, 2, 0, 1, 2], [1, 2, 0, 2, 0, 1]]),
         )
     )
 
@@ -39,7 +48,9 @@ def ring(n):
     x = t.ones((n, 1))
     edge_index = t.stack([t.arange(n), t.arange(n) + 1], dim=0)
     edge_index[1][-1] = 0
-    edge_index_flipped = t.cat([edge_index[1].unsqueeze(dim=0), edge_index[0].unsqueeze(dim=0)], dim=0)
+    edge_index_flipped = t.cat(
+        [edge_index[1].unsqueeze(dim=0), edge_index[0].unsqueeze(dim=0)], dim=0
+    )
     edge_index = t.cat([edge_index, edge_index_flipped], dim=1)
     return Graph(pyg.data.Data(x, edge_index))
 
@@ -53,8 +64,41 @@ def ring_with_attachments(num_nodes, shape_gen, num_shapes):
         # merge the networkx graphs ring and shape_graph,
         # connecting them at the node connecting_nodes[i]:
         r = r.disjoint_union(shape_graph)
-        r.add_edge(
-            connecting_nodes[i],
-            prev_nodes + 1
-        )
+        r.add_edge(connecting_nodes[i], prev_nodes + 1)
     return r
+
+
+def bhshapes(num_nodes, num_houses):
+    # Build the Barabasi-Albert graph:
+    random.seed(1234)
+    np.random.seed(1234)
+
+    ba_graph = nx.barabasi_albert_graph(num_nodes, 1)
+    node_labels = [0 for _ in range(num_nodes)]
+
+    # Select nodes to connect shapes:
+    connecting_nodes = np.random.permutation(num_nodes)[:num_houses]
+
+    # Connect houses to Barabasi-Albert graph:
+    for i in range(num_houses):
+        house_edges, house_labels = _raw_house()
+        house_edges += num_nodes
+        house_edges = [(e[0], e[1]) for e in house_edges]
+        # print(f"Connection house to node {int(connecting_nodes[i])}")
+        ba_graph.add_edges_from(
+            house_edges
+            + [
+                (int(connecting_nodes[i]), num_nodes),
+                (num_nodes, int(connecting_nodes[i])),
+            ]
+        )
+        num_nodes += 5
+        node_labels += house_labels
+
+    # print("DRAWING")
+    # nx.draw(ba_graph)
+    # plt.show()
+    # Identity is possibly a bad choice for feature here... TODO
+    nx.write_adjlist(ba_graph, f"training_runs/testing.adjlist")
+    np.save(f"training_runs/testing.labels", np.array(node_labels))
+    return ba_graph
